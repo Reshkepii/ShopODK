43a29dd0f69e8dbdac5ec82f8663e8c666dffc69
Rexhep <reshkepii@gmail.com>
2021-02-22 09:49:40 +0100

    Second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,6 +106,25 @@
       }
     });
   });
+$(".clients-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 2
+      },
+      768: {
+        items: 4
+      },
+      900: {
+        items: 6
+      }
+    }
+  });
+
+
+
 
   // Toggle .header-scrolled class to #header when page is scrolled
   $(window).scroll(function() {
